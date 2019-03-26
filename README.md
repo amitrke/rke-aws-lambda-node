36d6eb75c183428c8b0a57a651c367d02e3881b1
amitrke <amitrke@gmail.com>
2019-03-25 22:53:52 -0400

    Added database connectivity functions

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Roorkee AWS Lambda code
+# Roorkee.org AWS API & Lambda code
 
 [![CircleCI](https://circleci.com/gh/amitrke/rke-aws-lambda-node.svg?style=svg)](https://circleci.com/gh/amitrke/rke-aws-lambda-node)
 
 This repo hosts NodeJS code for roorkee.org AWS lambda functions.
 
-Develop stage URL https://vr1lptss5d.execute-api.us-east-1.amazonaws.com/dev/hello
+Develop stage URL https://api.aws.roorkee.org/dev/hello
--- handler.js
+++ handler.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const doc = require('dynamodb-doc');
+const dynamo = new doc.DynamoDB();
+
 module.exports.hello = async (event) => {
   return {
     statusCode: 200,
@@ -11,4 +14,40 @@ module.exports.hello = async (event) => {
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.microservice = (event, context, callback) => {
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+
+    const done = (err, res) => callback(null, {
+        statusCode: err ? '400' : '200',
+        body: err ? err.message : JSON.stringify(res),
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin':'*',
+            'Version':'17.10.15.4'
+        },
+    });
+
+    switch (event.httpMethod) {
+        case 'DELETE':
+            dynamo.deleteItem(JSON.parse(event.body), done);
+            break;
+        case 'GET':
+            if (event.queryStringParameters.id){
+                dynamo.getItem({ TableName: event.queryStringParameters.TableName, Key: {id: event.queryStringParameters.id}}, done);
+            }
+            else{
+                dynamo.scan({ TableName: event.queryStringParameters.TableName }, done);
+            }
+            break;
+        case 'POST':
+            dynamo.putItem(JSON.parse(event.body), done);
+            break;
+        case 'PUT':
+            dynamo.updateItem(JSON.parse(event.body), done);
+            break;
+        default:
+            done(new Error(`Unsupported method "${event.httpMethod}"`));
+    }
 };
