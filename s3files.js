de192a56d5b09a5bd72c8de8c361ba8fe0e2c40d
amitrke <amitrke@gmail.com>
2019-08-19 22:30:12 -0400

    Updated file upload base64

--- s3files.js
+++ s3files.js
@@ -1,36 +1,21 @@
 'use strict';
 const AWS = require('aws-sdk');
-const multipart = require('aws-lambda-multipart-parser');
-var s3 = new AWS.S3();
+var s3 = new AWS.S3( { params: {Bucket: 'www-static.aws.roorkee.org'} });
 
 module.exports.upload = async (event, context, callback) => {
-    const result = multipart.parse(event, true)
-    
-    let decodedImage = result.file.content.data;
-    var filePath = "uploads/"+result.file.filename;
-    var params = {
-      "Body": decodedImage,
-      "Bucket": "www-static.aws.roorkee.org",
-      "Key": filePath
-   };
-   s3.upload(params, function(err, data){
-      if(err) {
-          callback(err, null);
-      } else {
-            let response = {
-            "statusCode": 200,
-            "headers": {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin':'*',
-                'Access-Control-Allow-Headers': '*',
-                'Access-Control-Allow-Methods': 'GET, PUT, POST, OPTIONS, DELETE, HEAD',
-                "Access-Control-Allow-Credentials" : true,
-                'Version':'17.10.15.4'
-            },
-            "body": JSON.stringify(data),
-            "isBase64Encoded": false
-        };
-            callback(null, response);
+    buf = new Buffer(req.body.fileData.replace(/^data:image\/\w+;base64,/, ""),'base64')
+    var data = {
+        Key: req.body.fileName, 
+        Body: buf,
+        ContentEncoding: 'base64',
+        ContentType: 'image/jpeg'
+    };
+    s3Bucket.putObject(data, function(err, data){
+        if (err) { 
+            console.log(err);
+            console.log('Error uploading data: ', data); 
+        } else {
+            console.log('succesfully uploaded the image!');
         }
-   });
+    });
 };
