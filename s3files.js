83b7ddb9316f338dec789598bf3d319091a4da14
amitrke <amitrke@gmail.com>
2019-08-20 23:04:52 -0400

    File upload debugging

--- s3files.js
+++ s3files.js
@@ -4,16 +4,19 @@ var s3 = new AWS.S3( { params: {Bucket: 'www-static.aws.roorkee.org', region: 'u
 
 module.exports.upload = async (event) => {
     let parsedBody = JSON.parse(event.body);
-    let buf = new Buffer(parsedBody.fileData.replace(/^data:image\/\w+;base64,/, ""),'base64')
-    var data = {
-        Key: parsedBody.fileName, 
+    let buf = new Buffer(parsedBody.data.replace(/^data:image\/\w+;base64,/, ""),'base64');
+
+    var dimensions = sizeOf(buf);
+    console.log(dimensions.width, dimensions.height);
+
+    var params = {
+        Key: parsedBody.name, 
         Body: buf,
-        ContentEncoding: 'base64',
-        ContentType: 'image/jpeg'
+        ContentEncoding: parsedBody.type
     };
     
     try{
-        var resp = await s3.putObject(data).promise();
+        var resp = await s3.putObject(params).promise();
         console.log(JSON.stringify(resp));
         return {
             statusCode: 200,
@@ -27,5 +30,4 @@ module.exports.upload = async (event) => {
             body: JSON.stringify(err)
         }
     }
-    
 };
